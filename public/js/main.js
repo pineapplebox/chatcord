9c02c716871c39db9ce365d859e73057bb749316
pineapplebox <37129442+pineapplebox@users.noreply.github.com>
2021-06-07 11:25:22 -0400

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -19,7 +19,7 @@ tinymce.init({
 	setup: function (editor) {
     editor.ui.registry.addButton("mySendButton", {
       tooltip: "Send Message",
-	  text: '<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Font_Awesome_5_solid_paper-plane.svg/1200px-Font_Awesome_5_solid_paper-plane.svg.png" alt="" width="14" height="14"> Send',
+	  text: '<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Font_Awesome_5_solid_paper-plane.svg/1200px-Font_Awesome_5_solid_paper-plane.svg.png" alt="" width="14px" height="14px"> Send',
       onAction: function () {
         if (!editor.getContent()) {
           return false;
