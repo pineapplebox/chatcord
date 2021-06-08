ba5080791a26e70a4367d9e2fe5e1e0b6deb6e2c
pineapplebox <37129442+pineapplebox@users.noreply.github.com>
2021-06-08 17:29:41 -0400

    Update noenter.js

--- public/js/noenter.js
+++ public/js/noenter.js
@@ -1,1 +1,1 @@
-window.addEventListener('keydown',function(e){if(e.which == 13 && e.shiftKey){return false;}else if(e.keyCode==13){e.preventDefault();return false;}},true);
+window.addEventListener('keydown',function(e){if(e.which == 13 && e.shiftKey){return false;}else if(e.keyCode==13){e.preventDefault();return true;}},true);
